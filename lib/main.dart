7ab566bae94c73d37084cec78dcc768e433ebf27
subki1992 <ahmad.subki1992@utmmataram.ac.id>
2024-10-22 01:39:02 +0000

    Ini data baru 'Title Facebook'

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  // Perbaiki penulisan 'class' dan 'extends'
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    // Perbaiki penulisan 'Widget'
+    return MaterialApp(home: MyHome()); // Hapus karakter tidak perlu
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Perbaiki penulisan 'Widget'
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+          title: Text("Facebook"),
+          backgroundColor: Colors.green), // Hapus karakter tidak perlu
+
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Image(
+              image: NetworkImage(
+                  "https://i.ytimg.com/vi/J9JAY0JwZK4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC9AlSUqhh3XTxJhh5sGbeG92F73Q")),
+          Text(
+            "Es Campur",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(
+              "Es campur adalah salah satu minuman khas Indonesia, dibuat dengan mencampurkan berbagai jenis bahan dalam sirop manis. Bahan yang dijadikan bahan biasanya berasa manis atau masam. Es campur dapat dijumpai di berbagai daerah di Indonesia dengan rasa dan bahan yang berbeda."),
+          Row(
+            children: [
+              Icon(Icons.phone_android_rounded, size: 50),
+              Text("089703652230"),
+              Icon(
+                Icons.share,
+                size: 50,
+                color: Colors.blue,
+              )
+            ],
+          ),
+          Image(
+              image: NetworkImage(
+                  "https://i.ytimg.com/vi/J9JAY0JwZK4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLC9AlSUqhh3XTxJhh5sGbeG92F73Q")),
+          Text(
+            "Bakso Urat",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(
+              "Es campur adalah salah satu minuman khas Indonesia, dibuat dengan mencampurkan berbagai jenis bahan dalam sirop manis. Bahan yang dijadikan bahan biasanya berasa manis atau masam. Es campur dapat dijumpai di berbagai daerah di Indonesia dengan rasa dan bahan yang berbeda."),
+          Row(
+            children: [
+              Icon(Icons.phone_android_rounded, size: 50),
+              Text("089703652230"),
+              Icon(
+                Icons.share,
+                size: 50,
+                color: Colors.blue,
+              )
+            ],
+          )
+        ]),
       ),
     );
   }
